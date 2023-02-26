bb0d0f38b6efd95682ed14ae11082f8c3cda6ee4
thatshinji <li2.y@foxmail.com>
2023-02-26 23:44:02 +0800

    feat: dynamic append script into html

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer as createViteDevServer } from 'vite'
 import { pluginHtml } from './plugin-wille/html'
+import pluginReact from '@vitejs/plugin-react'
 
 export const createDevServer = async (root = process.cwd()) => {
   return createViteDevServer({
     root,
-    plugins: [pluginHtml()]
+    plugins: [pluginHtml(), pluginReact()]
   })
 }
